66363371efc28d74a32b1519d94736f4c430b320
Andrea Lamparelli <a.lamparelli95@gmail.com>
2025-08-07 19:04:42 +0200

    Remove TimerService resource
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- backend/src/main/java/org/cajun/navy/model/responder/ResponderSimulatorTask.java
+++ backend/src/main/java/org/cajun/navy/model/responder/ResponderSimulatorTask.java
@@ -2,15 +2,9 @@ package org.cajun.navy.model.responder;
 
 import org.cajun.navy.model.mission.MissionEntity;
 import org.cajun.navy.service.MissionService;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.ejb.ScheduleExpression;
 import javax.ejb.Singleton;
 import javax.ejb.Timeout;
-import javax.ejb.TimerConfig;
-import javax.ejb.TimerService;
 import javax.inject.Inject;
 import java.util.List;
 import java.util.logging.Logger;
@@ -20,27 +14,16 @@ public class ResponderSimulatorTask {
 
     private static final Logger logger = Logger.getLogger(ResponderSimulatorTask.class.getName());
 
-    @Resource
-    TimerService timerService;
+//    @Resource
+//    TimerService timerService;
 
     @Inject
     MissionService missionService;
 
-    @Inject
-    @ConfigProperty(name = "responder.simulator.task.active", defaultValue = "true")
-    private boolean active;
+//    @Inject
+//    @ConfigProperty(name = "responder.simulator.task.active", defaultValue = "true")
+//    private boolean active;
 
-    @PostConstruct
-    void init() {
-        if (active) {
-            ScheduleExpression expression = new ScheduleExpression().second("*/5").minute("*").hour("*");
-            TimerConfig timerConfig = new TimerConfig();
-            timerConfig.setPersistent(true);
-            timerService.createCalendarTimer(expression, timerConfig);
-        } else {
-            logger.info("Disable ResponderSimulatorTask");
-        }
-    }
 
     @Timeout
     public void doMoveResponderLocation(){
